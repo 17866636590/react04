0e517603ea90ed6a595857a938e292a54adfd9a6
17866636590 <455116351@qq.com>
2020-11-20 15:34:26 +0800

    'last'

---
+++ src/setUpProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+module.exports=function(app){
+    app.use('/api', createProxyMiddleware({
+         target: 'https://m.maoyan.com', 
+         changeOrigin: true ,
+         pathRewrite:{
+            "^/api":''
+         }
+        }));
+app.listen(3000);
+}
